c8d50223fbf5493aa253ea6c04a7feb70ef4960a
Zaland Khan <zalandkhan1@gmail.com>
2021-10-13 20:41:49 -0700

    Style and setup forgot password page

--- app/auth/pages/forgot-password.tsx
+++ app/auth/pages/forgot-password.tsx
@@ -1,48 +1,103 @@
-import { BlitzPage, useMutation } from "blitz"
+import {
+  Avatar,
+  CssBaseline,
+  TextField,
+  Link,
+  Box,
+  Typography,
+  Container,
+  Alert,
+} from "@mui/material"
+import { FitnessCenter } from "@mui/icons-material"
+import { LoadingButton } from "@mui/lab"
+import { Form, FormikProvider, useFormik } from "formik"
+import { BlitzPage, useMutation, validateZodSchema, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
-import { LabeledTextField } from "app/core/components/LabeledTextField"
-import { Form, FORM_ERROR } from "app/core/components/Form"
 import { ForgotPassword } from "app/auth/validations"
 import forgotPassword from "app/auth/mutations/forgotPassword"
 
 const ForgotPasswordPage: BlitzPage = () => {
   const [forgotPasswordMutation, { isSuccess }] = useMutation(forgotPassword)
 
-  return (
-    <div>
-      <h1>Forgot your password?</h1>
+  const formik = useFormik({
+    initialValues: { email: "" },
+    validate: validateZodSchema(ForgotPassword),
+    onSubmit: async (values, { setErrors }) => {
+      console.log("in here")
+      try {
+        await forgotPasswordMutation(values)
+      } catch (error: any) {
+        setErrors({ afterSubmit: "Sorry, we had an unexpected error. Please try again." })
+      }
+    },
+  })
 
-      {isSuccess ? (
-        <div>
-          <h2>Request Submitted</h2>
-          <p>
-            If your email is in our system, you will receive instructions to reset your password
-            shortly.
-          </p>
-        </div>
-      ) : (
-        <Form
-          submitText="Send Reset Password Instructions"
-          schema={ForgotPassword}
-          initialValues={{ email: "" }}
-          onSubmit={async (values) => {
-            try {
-              await forgotPasswordMutation(values)
-            } catch (error: any) {
-              return {
-                [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
-              }
-            }
-          }}
-        >
-          <LabeledTextField name="email" label="Email" placeholder="Email" />
-        </Form>
-      )}
-    </div>
+  const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <FitnessCenter />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Forgot your password?
+        </Typography>
+
+        {isSuccess ? (
+          <>
+            <Typography sx={{ marginTop: 3 }}>
+              Request submitted! If your email is in our system, you will receive instructions to
+              reset your password shortly.
+            </Typography>
+            <Link
+              href={Routes.LoginPage().pathname}
+              variant="body2"
+              underline="none"
+              sx={{ marginTop: 2 }}
+            >
+              Return to login
+            </Link>
+          </>
+        ) : (
+          <FormikProvider value={formik}>
+            <Form noValidate onSubmit={handleSubmit}>
+              {errors.afterSubmit && <Alert severity="error">{errors.afterSubmit}</Alert>}
+              <TextField
+                margin="normal"
+                fullWidth
+                type="email"
+                label="Email"
+                {...getFieldProps("email")}
+                error={Boolean(touched.email && errors.email)}
+                helperText={touched.email && errors.email}
+              />
+              <LoadingButton
+                loading={isSubmitting}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                reset password
+              </LoadingButton>
+            </Form>
+          </FormikProvider>
+        )}
+      </Box>
+    </Container>
   )
 }
 
 ForgotPasswordPage.redirectAuthenticatedTo = "/"
-ForgotPasswordPage.getLayout = (page) => <Layout title="Forgot Your Password?">{page}</Layout>
+ForgotPasswordPage.getLayout = (page) => <Layout title="Forgot your password?">{page}</Layout>
 
 export default ForgotPasswordPage
