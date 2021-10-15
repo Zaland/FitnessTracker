2c9e5b6960a70465735aea1a7fb6725ef311fe34
Zaland Khan <zalandkhan1@gmail.com>
2021-10-15 15:35:52 -0700

    Add eslint support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,24 @@
 module.exports = {
-  extends: ["blitz"],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/display-name": "off",
+  },
 }
