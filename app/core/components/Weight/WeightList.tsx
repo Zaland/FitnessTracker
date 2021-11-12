bd85a2f55fb57ff009143e37483203c1d3d707fb
Zaland Khan <zalandkhan1@gmail.com>
2021-11-11 20:19:35 -0800

    Use a table for the weights list instead

--- app/core/components/Weight/WeightList.tsx
+++ app/core/components/Weight/WeightList.tsx
@@ -1,4 +1,13 @@
-import { TextField, Grid, Typography, Paper, ToggleButton, ToggleButtonGroup } from "@mui/material"
+import {
+  Typography,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material"
 import { format } from "date-fns"
 import { WeightProps } from "./Weight"
 
@@ -12,49 +21,28 @@ export const WeightList = ({ weights }: WeightListProps) => (
       <Typography component="h1" variant="h5" align="center">
         Weight list
       </Typography>
-      {weights.map((weight) => (
-        <Grid container spacing={4} key={weight.id}>
-          <Grid item sm={7}>
-            <Grid container spacing={2}>
-              <Grid item sm={8}>
-                <TextField
-                  margin="normal"
-                  label="Weight"
-                  fullWidth
-                  disabled
-                  InputLabelProps={{ shrink: true }}
-                  value={weight.amount}
-                />
-              </Grid>
-              <Grid item sm={4}>
-                <ToggleButtonGroup
-                  fullWidth
-                  exclusive
-                  color="primary"
-                  disabled
-                  sx={{ marginTop: 2.5 }}
-                  value={weight.isTypePounds ? "lb" : "kg"}
-                >
-                  <ToggleButton value="lb" defaultChecked>
-                    lb
-                  </ToggleButton>
-                  <ToggleButton value="kg">kg</ToggleButton>
-                </ToggleButtonGroup>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item sm={5}>
-            <TextField
-              margin="normal"
-              label="Date"
-              fullWidth
-              disabled
-              InputLabelProps={{ shrink: true }}
-              value={format(weight.logDate, "MM/dd/yyyy")}
-            />
-          </Grid>
-        </Grid>
-      ))}
+      <TableContainer component={Paper} elevation={5} sx={{ marginTop: 2 }}>
+        <Table aria-label="table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Date</TableCell>
+              <TableCell align="center">Weight</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {weights.map((row) => (
+              <TableRow key={row.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                <TableCell align="center" component="th" scope="row">
+                  {format(row.logDate, "LLL d, yyyy")}
+                </TableCell>
+                <TableCell align="center">{`${row.amount} ${
+                  row.isTypePounds ? "lb" : "kg"
+                }`}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Paper>
   </>
 )
